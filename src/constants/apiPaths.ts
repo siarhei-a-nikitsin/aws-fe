651316b9633294d7e0916a3eafdf63244a49c46e
siarhei1988 <siarhei.nikitsin@onde.app>
2020-11-01 11:56:21 +0300

    feat: Task 3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,30 @@
+import { generatePath } from "react-router";
 
-const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+enum Environment {
+  Development = "dev"
+}
+
+const services = {
+  products: {
+    baseUrl: "products",
+    methods: {
+      allProducts: "",
+      productById: "/:id"
+    }
+  }
+}
+
+const getUrl = (service: string, method: string, params: { [key: string]: any } = {}, environment = Environment.Development) =>
+  generatePath(`/${environment}/${service}${method}`, params);
+
+const ApiRoutes = {
+  products: getUrl(services.products.baseUrl, services.products.methods.allProducts),
+  productById: (productId: string) => getUrl(services.products.baseUrl, services.products.methods.productById, { id: productId }),
+
+  product: 'https://89u84drm88.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
-export default API_PATHS;
+export default ApiRoutes;
