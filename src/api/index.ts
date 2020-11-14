f2bc8037d3cd800919043ae03fba25726d284a94
siarhei1988 <siarhei.nikitsin@onde.app>
2020-11-14 21:06:02 +0300

    feat: Task 5

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,14 +3,7 @@ import Axios from 'axios';
 const BACKEND_BASE_URL = 'https://89u84drm88.execute-api.eu-west-1.amazonaws.com';
 
 export const axios = Axios.create({
-  baseURL: BACKEND_BASE_URL,
-  withCredentials: false,
-  responseType: 'json',
-  headers: { 
-    'Access-Control-Allow-Origin': '*',
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  }
+  baseURL: BACKEND_BASE_URL
 });
 
 export default axios;
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -11,7 +11,13 @@ const services = {
       allProducts: "",
       productById: "/:id"
     }
-  }
+  },
+  importProducts: {
+    baseUrl: "import",
+    methods: {
+      getSignedUrl: "?:name"
+    },
+  },
 }
 
 const getUrl = (service: string, method: string, params: { [key: string]: any } = {}, environment = Environment.Development) =>
@@ -21,9 +27,11 @@ const ApiRoutes = {
   products: getUrl(services.products.baseUrl, services.products.methods.allProducts),
   productById: (productId: string) => getUrl(services.products.baseUrl, services.products.methods.productById, { id: productId }),
 
+  // import: (fileName: string) => getUrl(services.importProducts.baseUrl, services.importProducts.methods.getSignedUrl, { name: fileName }),
+  import: 'https://y4tw06shu3.execute-api.eu-west-1.amazonaws.com/dev',
+
   product: 'https://89u84drm88.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
